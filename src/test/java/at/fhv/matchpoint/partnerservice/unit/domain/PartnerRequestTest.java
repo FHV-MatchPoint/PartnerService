c64fc7293823323b7c88a79ff040f217426230cf
Jakob <jaypan44@gmail.com>
2023-04-23 14:09:49 +0200

    updated tests

--- src/test/java/at/fhv/matchpoint/partnerservice/unit/domain/PartnerRequestTest.java
+++ src/test/java/at/fhv/matchpoint/partnerservice/unit/domain/PartnerRequestTest.java
@@ -2,39 +2,70 @@ package at.fhv.matchpoint.partnerservice.unit.domain;
 
 import at.fhv.matchpoint.partnerservice.domain.model.PartnerRequest;
 import at.fhv.matchpoint.partnerservice.domain.model.RequestState;
+import at.fhv.matchpoint.partnerservice.events.AggregateType;
 import at.fhv.matchpoint.partnerservice.events.RequestAcceptedEvent;
 import at.fhv.matchpoint.partnerservice.events.RequestInitiatedEvent;
+import io.quarkus.test.junit.QuarkusTest;
+
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@QuarkusTest
 public class PartnerRequestTest {
 
-//    @Test
-//    public void testApplyRequestCreatedEvent() {
-//        RequestInitiatedEvent event = new RequestInitiatedEvent();
-//        PartnerRequest partnerRequest = new PartnerRequest();
-//        partnerRequest.apply(event);
-//        assertEquals("1", partnerRequest.getPartnerRequestId());
-//        assertEquals("Markomannen", partnerRequest.getOwnerId());
-//        assertEquals("Panther", partnerRequest.getClubId());
-//        assertEquals(LocalDate.now(), partnerRequest.getDate());
-//        assertEquals(LocalTime.NOON, partnerRequest.getStartTime());
-//        assertEquals(LocalTime.MIDNIGHT, partnerRequest.getEndTime());
-//        assertEquals(RequestState.INITIATED, partnerRequest.getState());
-//    }
-//
-//    @Test
-//    public void testApplyRequestAcceptedEvent() {
-//        RequestAcceptedEvent event = new RequestAcceptedEvent();
-//        PartnerRequest partnerRequest = new PartnerRequest();
-//        partnerRequest.apply(event);
-//        assertEquals("Helvetier", partnerRequest.getPartnerId());
-//        assertNotNull(partnerRequest.getStartTime());
-//        assertEquals(LocalTime.MIDNIGHT, partnerRequest.getEndTime());
-//        assertEquals(RequestState.ACCEPTED, partnerRequest.getState());
-//    }
+    final String AGGREGATE_ID = "Angeln";
+    final String OWNER_ID = "Markomannen";
+    final String PARTNER_ID = "Helvetier";
+    final String CLUD_ID = "Gallier";
+    final LocalDate DATE = LocalDate.now();
+    final LocalTime START_TIME = LocalTime.NOON;
+    final LocalTime END_TIME = LocalTime.MIDNIGHT;
+
+   @Test
+   public void testApplyRequestInitiatedEvent() {
+       RequestInitiatedEvent event = new RequestInitiatedEvent();
+       event.aggregateId = AGGREGATE_ID;
+       event.aggregateType = AggregateType.PARTNERREQUEST;
+       event.createdAt = LocalDateTime.now();
+       event.ownerId = OWNER_ID;
+       event.tennisClubId = CLUD_ID;
+       event.date = DATE;
+       event.startTime = START_TIME;
+       event.endTime = END_TIME;
+       PartnerRequest partnerRequest = new PartnerRequest();
+       partnerRequest.apply(event);
+       assertEquals(AGGREGATE_ID, partnerRequest.getPartnerRequestId());
+       assertEquals(OWNER_ID, partnerRequest.getOwnerId());
+       assertEquals(CLUD_ID, partnerRequest.getClubId());
+       assertEquals(DATE, partnerRequest.getDate());
+       assertEquals(START_TIME, partnerRequest.getStartTime());
+       assertEquals(END_TIME, partnerRequest.getEndTime());
+       assertEquals(RequestState.INITIATED, partnerRequest.getState());
+   }
+
+   @Test
+   public void testApplyRequestAcceptedEvent() {
+       RequestAcceptedEvent event = new RequestAcceptedEvent();
+       event.aggregateId = AGGREGATE_ID;
+       event.aggregateType = AggregateType.PARTNERREQUEST;
+       event.createdAt = LocalDateTime.now();
+       event.partnerId = PARTNER_ID;
+       event.startTime = START_TIME;
+       event.endTime = END_TIME;
+       PartnerRequest partnerRequest = new PartnerRequest();
+       partnerRequest.apply(event);
+       assertNull(partnerRequest.getPartnerRequestId());
+       assertNull(partnerRequest.getOwnerId());
+       assertNull(partnerRequest.getClubId());
+       assertNull(partnerRequest.getDate());
+       assertEquals(PARTNER_ID, partnerRequest.getPartnerId());
+       assertEquals(START_TIME, partnerRequest.getStartTime());
+       assertEquals(END_TIME, partnerRequest.getEndTime());
+       assertEquals(RequestState.ACCEPTED, partnerRequest.getState());
+   }
 }
